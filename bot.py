a99d72aa195653169c1c361a492343d34b198c2b
taran2024proton <taran2024@proton.me>
2025-12-14 17:01:32 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -392,6 +392,9 @@ def root():
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
+    import os 
+    port = int(os.environ.get("PORT", 5000))
+    
     bot.delete_webhook()
     bot.set_webhook(WEBHOOK_URL)
     app.run(host="0.0.0.0", port=port)
